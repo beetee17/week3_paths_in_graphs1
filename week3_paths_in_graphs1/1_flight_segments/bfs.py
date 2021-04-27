d6f23f5e729d2e2077144dd3796ce7b302f15ff6
beetee17 <brandon.thio@yahoo.com.sg>
2021-04-27 10:01:07 +0800

    init

--- week3_paths_in_graphs1/1_flight_segments/bfs.py
+++ week3_paths_in_graphs1/1_flight_segments/bfs.py
@@ -1,21 +1,137 @@
 #Uses python3
 
 import sys
-import queue
+from collections import defaultdict
+from queue import Queue
 
-def distance(adj, s, t):
-    #write your code here
-    return -1
+class Vertex():
+    def __init__(self, index):
+        self.index = index
+
+        self.visited = 0
+
+        self.pre = None
+        self.post = None
+
+        # for computing of shortest path tree
+        self.dist = -1
+
+        # use boolean to keep track of if vertex has been discovered instead of setting to infinity
+        self.dist_defined = False
+
+        # for computing layer of vertex in tree and retracing shortest path  
+        self.prev = None
+
+
+class Graph():
+    def __init__(self, edges, vertices):
+
+        # create a dict with vertex as key and list of its neighbours as values
+        self.adj = defaultdict(list)
+
+        self.edges = edges
+
+        # for an udirected graph, b is adjacent to a and vice versa
+        for (a, b) in edges:
+            self.adj[vertices[a-1]].append(vertices[b-1])
+            self.adj[vertices[b-1]].append(vertices[a-1])
+                    
+        self.vertices = vertices
+   
+        self.acyclic = True
+
+        self.clock = 1
+    
+    def previst(self, v):
+    
+        v.pre = self.clock
+        self.clock += 1
+        
+    def postvist(self, v):
+
+        v.post = self.clock
+        self.clock += 1
+
+        v.visited = 1
+
+    def explore(self, v):
+
+        v.visited = -1
+        # -1 indicates that the neighbours of this vertex is currently being explored in the recursion sub routines
+        # pre-vist block
+        self.previst(v)
+
+        # explore each neighbour of the vertex 
+        for neighbour in self.adj[v]:
+
+            # if the neighbours point back to the original vertex, we have found a cycle (there is a series of edges which start from v and end at v)
+            if neighbour.visited == -1:
+                self.acyclic = False
+
+            if neighbour.visited == 0:
+                self.explore(neighbour)
+
+        # post-visit block
+        self.postvist(v)
+
+    def make_shortest_path_tree(self, s):
+        """compute the shortest path tree given the starting vertex's index"""
+
+        s = vertices[s]
+
+        # init starting vertex distance to 0
+        s.dist = 0
+        s.dist_defined = True
+
+        # create queue for processing of neighbours
+        Q = Queue(maxsize=2*len(self.edges) + 1)
+
+        # init the first layer of the tree
+        for neighbour in self.adj[s]:
+            neighbour.prev = s
+            Q.put_nowait(neighbour)
+
+        # iterate until all reachable vertices have been discovered
+        while not Q.empty():
+
+            # dequeue
+            v = Q.get_nowait()
+
+            if not v.dist_defined:
+
+                # set distance if it has not been previously discovered
+                v.dist = v.prev.dist + 1
+                v.dist_defined = True
+                # print(v.index, v.prev.index, v.dist)
+
+                # enqueue all its undiscovred neighbours
+                for neighbour in self.adj[v]:
+
+                    # do not allow overwriting of prev
+                    if not neighbour.prev:
+                        neighbour.prev = v
+
+                    Q.put_nowait(neighbour)
+
+
+def distance(graph, s, t):
+    """Given an undirected graph with 𝑛 vertices and 𝑚 edges and two vertices 𝑢 and 𝑣, compute the length of a shortest path between 𝑢 and 𝑣 (that is, the minimum number of edges in a path from 𝑢 to 𝑣)
+    
+    Output the minimum number of edges in a path from 𝑢 to 𝑣, or −1 if there is no path"""
+
+    graph.make_shortest_path_tree(s)
+    return graph.vertices[t].dist
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
+
+    vertices = [Vertex(i) for i in range(n)]
+
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
+
     s, t = data[2 * m] - 1, data[2 * m + 1] - 1
-    print(distance(adj, s, t))
+
+    print(distance(Graph(edges, vertices), s, t))
--- week3_paths_in_graphs1/2_bipartite/bipartite.py
+++ week3_paths_in_graphs1/2_bipartite/bipartite.py
@@ -1,20 +1,154 @@
 #Uses python3
 
 import sys
-import queue
+from collections import defaultdict
+from queue import Queue
 
-def bipartite(adj):
-    #write your code here
-    return -1
+class Vertex():
+    def __init__(self, index):
+        self.index = index
 
+        self.visited = 0
+
+        self.pre = None
+        self.post = None
+
+        # for computing of shortest path tree
+        self.dist = -1
+
+        # to check which tree vertex is in (if graph has multiple trees)
+        self.origin = None
+
+        # use boolean to keep track of if vertex has been discovered instead of setting to infinity
+        self.dist_defined = False
+
+        # for computing layer of vertex in tree and retracing shortest path  
+        self.prev = None
+
+
+class Graph():
+    def __init__(self, edges, vertices):
+
+        # create a dict with vertex as key and list of its neighbours as values
+        self.adj = defaultdict(list)
+
+        self.edges = edges
+
+        # for an udirected graph, b is adjacent to a and vice versa
+        for (a, b) in edges:
+            self.adj[vertices[a-1]].append(vertices[b-1])
+            self.adj[vertices[b-1]].append(vertices[a-1])
+                    
+        self.vertices = vertices
+   
+        self.acyclic = True
+
+        self.clock = 1
+    
+    def previst(self, v):
+    
+        v.pre = self.clock
+        self.clock += 1
+        
+    def postvist(self, v):
+
+        v.post = self.clock
+        self.clock += 1
+
+        v.visited = 1
+
+    def explore(self, v):
+
+        v.visited = -1
+        # -1 indicates that the neighbours of this vertex is currently being explored in the recursion sub routines
+        # pre-vist block
+        self.previst(v)
+
+        # explore each neighbour of the vertex 
+        for neighbour in self.adj[v]:
+
+            # if the neighbours point back to the original vertex, we have found a cycle (there is a series of edges which start from v and end at v)
+            if neighbour.visited == -1:
+                self.acyclic = False
+
+            if neighbour.visited == 0:
+                self.explore(neighbour)
+
+        # post-visit block
+        self.postvist(v)
+
+    def make_shortest_path_tree(self, s):
+        """compute the shortest path tree given the starting vertex's index"""
+
+        s = vertices[s]
+
+        # init starting vertex distance to 0
+        s.dist = 0
+        s.origin = s
+        s.dist_defined = True
+
+        # create queue for processing of neighbours
+        Q = Queue(maxsize=2*len(self.edges) + 1)
+
+        # init the first layer of the tree
+        for neighbour in self.adj[s]:
+            neighbour.prev = s
+            neighbour.origin = s
+            Q.put_nowait(neighbour)
+
+        # iterate until all reachable vertices have been discovered
+        while not Q.empty():
+
+            # dequeue
+            v = Q.get_nowait()
+
+            if not v.dist_defined:
+
+                # set distance if it has not been previously discovered
+                v.dist = v.prev.dist + 1
+                v.dist_defined = True
+                # print(v.index, v.prev.index, v.dist)
+
+                # enqueue all its undiscovred neighbours
+                for neighbour in self.adj[v]:
+
+                    # do not allow overwriting of prev
+                    if not neighbour.prev:
+                        neighbour.prev = v
+
+                    neighbour.origin = s
+                    Q.put_nowait(neighbour)
+
+def bipartite(graph):
+    
+    graph.make_shortest_path_tree(0)
+
+    # ensure all vertices are in some shortest path tree
+    for v in graph.vertices:
+        if not v.dist_defined:
+            graph.make_shortest_path_tree(v.index)
+
+
+    for (a, b) in graph.edges:
+        # print(graph.vertices[a-1].dist, graph.vertices[b-1].dist)
+        # print(graph.vertices[a-1].origin.index, graph.vertices[b-1].origin.index)
+        if graph.vertices[a-1].origin == graph.vertices[b-1].origin and graph.vertices[a-1].dist == graph.vertices[b-1].dist:
+            # if there exists an edge between two vertices that are in the same layer of the same shortest path tree, the graph is not bipartite
+            return 0
+
+    return 1
+
+
+    
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
+
+    vertices = [Vertex(i) for i in range(n)]
+
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    adj = [[] for _ in range(n)]
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(bipartite(adj))
+
+
+    print(bipartite(Graph(edges, vertices)))
